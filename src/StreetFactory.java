cbde1b1f8b39deeaec424f27f0cbd51eca39401b
DatManRandom <Osama.i.Faza@gmail.com>
2021-02-25 20:11:08 +0200

    StreetFactory.java

---
+++ src/StreetFactory.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreetFactory {
+    private static List<Street> streetList = new ArrayList<>();
+
+    protected void addStreet(String name, int startInt, int endInt) {
+        streetList.add(new Street(startInt, endInt, name));
+    }
+
+
+    private class Street {
+        private int startIntersection;
+        private int endIntersection;
+        private String streetName;
+
+        public int getStartIntersection() {
+            return startIntersection;
+        }
+
+        public void setStartIntersection(int startIntersection) {
+            this.startIntersection = startIntersection;
+        }
+
+        public int getEndIntersection() {
+            return endIntersection;
+        }
+
+        public void setEndIntersection(int endIntersection) {
+            this.endIntersection = endIntersection;
+        }
+
+        public String getStreetName() {
+            return streetName;
+        }
+
+        public void setStreetName(String streetName) {
+            this.streetName = streetName;
+        }
+
+        public Street(int startIntersection, int endIntersection, String streetName) {
+            this.startIntersection = startIntersection;
+            this.endIntersection = endIntersection;
+            this.streetName = streetName;
+
+        }
+    }
+}
