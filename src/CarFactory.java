1b55fb4c01e073482c1a72219008fe181c2ccdec
DatManRandom <Osama.i.Faza@gmail.com>
2021-02-25 20:15:53 +0200

    CarFactory.java

---
+++ src/CarFactory.java
@@ -0,0 +1,28 @@
+import javax.swing.event.ListDataListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CarFactory {
+    private List<Car> carList = new ArrayList<>();
+
+    protected void addCar(String[] streets){
+        carList.add(new Car(List.of(streets)));
+    }
+
+    private class Car{
+        List<String> streets = new ArrayList<>();
+
+        public Car(List<String> streets) {
+            this.streets = streets;
+        }
+
+        public List<String> getStreets() {
+            return streets;
+        }
+
+        public void setStreets(List<String> streets) {
+            this.streets = streets;
+        }
+    }
+}
